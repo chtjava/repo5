71876938dadd5fde938e289f1584a4a7d902da93
cht <chtjava@163.com>
2020-11-04 16:14:19 +0800

    新建项目

---
+++ git_test_idea/src/com/deyuan/Main.java
@@ -0,0 +1,12 @@
+package com.deyuan;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("s111111111111111111111");
+        System.out.println("s111111111111111111111");
+        System.out.println("s111111111111111111111");
+        System.out.println("s111111111111111111111");
+        System.out.println("s111111111111111111111");
+    }
+}
